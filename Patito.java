044f961ab2c1c28cd3739e6bbdc127094c565242
AvovA <luis.enrique.gonzalez.sanchezz@gmail.com>
2023-05-04 18:37:17 -0600

    mi primer commit :qw

---
+++ Patito.java
@@ -0,0 +1,10 @@
+//Esta es la clase patito
+public class Patito {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Esta es la Primer version de Patito");
+		System.out.println("1");
+	}
+
+}
